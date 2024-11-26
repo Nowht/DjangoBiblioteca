906301a299f1c4fae72f2b53968ce72341b5e63b
Nowht <humbertogonzalez5445@gmail.com>
2024-11-26 15:54:03 -0600

    subiendo a render 4

--- manage.py
+++ manage.py
@@ -2,13 +2,30 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.contrib.auth.models import User
 
+def create_superuser():
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+
+    if username and password and email:
+        if not User.objects.filter(username=username).exists():
+            print(f"Creando SuperUsuario:{username}")
+            User.objects.create_superuser(username=username, password=password, email=email)
+        else:
+            print("SuperUsuario ya existe D:")
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitio.settings')
     try:
         from django.core.management import execute_from_command_line
+        import django
+        django.setup()
+        create_superuser()
+    except Exception as e:
+        print(f"Error Creando el SuperUsuario:{e}")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
